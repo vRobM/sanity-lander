7225538b2954bbfa1959e5a348183cb4164161da
Rob <vRobM@users.noreply.github.com>
2023-01-29 00:58:09 -0800

    Update schema.ts

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -2,5 +2,7 @@ import { SchemaTypeDefinition } from 'sanity'
 import landerEmail from './landerEmail'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [
+    landerEmail
+  ],
 }
